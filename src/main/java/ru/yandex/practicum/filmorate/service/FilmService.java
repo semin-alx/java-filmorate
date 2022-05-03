895327d6e93997ece096e86b3d7eac3d1eb116ca
semin <semin.aleksey1@yandex.ru>
2022-05-03 11:02:58 +0300

    fix 1

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.BaseStorage;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,16 +13,25 @@ import java.util.stream.Collectors;
 @Service
 public class FilmService extends BaseService<Film> {
 
-    public FilmService(BaseStorage<Film> storage) {
-        super(storage);
+    private BaseStorage<User> userStorage;
+
+    private void checkUserId(int id) {
+        userStorage.getItemById(id); // Если такого id нет, будет исключение
+    }
+
+    public FilmService(BaseStorage<Film> filmStorage, BaseStorage<User> userStorage) {
+        super(filmStorage);
+        this.userStorage = userStorage;
     }
 
     public void like(int filmId, int userId) {
+        checkUserId(userId);
         Film film = getStorage().getItemById(filmId);
         film.addLike(userId);
     }
 
     public void removeLike(int filmId, int userId) {
+        checkUserId(userId);
         Film film = getStorage().getItemById(filmId);
         film.removeLike(userId);
     }
--- src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryBaseStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryBaseStorage.java
@@ -54,4 +54,5 @@ public class InMemoryBaseStorage<T extends Item> implements BaseStorage<T> {
 
         return item;
     }
+
 }
